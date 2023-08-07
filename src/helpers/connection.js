94e8ffdc5b7df13611e2621b53fd18d6bc9ce50d
Andrew Hany <andrewhany260@gmail.com>
2023-08-07 21:07:26 +0300

    eslint formatting

--- src/helpers/connection.js
+++ src/helpers/connection.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const keys = require('../keys');
 // connect to mongodb
 const connect = async () => {
   try {
     const db = await mongoose.connect(keys.mongodbUrl, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log("Successfully established connection to database");
+    console.log('Successfully established connection to database');
     return db;
   } catch (err) {
-    console.error("Unable to connect to database", err);
+    console.error('Unable to connect to database', err);
   }
 };
 
 module.exports = {
-  connect,
+  connect
 };
