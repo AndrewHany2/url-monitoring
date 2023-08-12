039c4f9801a8bdc7033f71208496b27adf058883
Andrew Hany <andrewhany260@gmail.com>
2023-08-12 16:30:20 +0300

    ping url with tcp protocol

--- src/services/urlCheck.service.js
+++ src/services/urlCheck.service.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const https = require('https');
 const { markReport } = require('./report.service');
+const net = require('net');
 
 async function pingUrl (check) {
   const { url, protocol, path, port, timeout, ignoreSSL, authentication, httpHeaders, assert } = check;
@@ -49,7 +50,51 @@ async function pingUrl (check) {
       await markReport(check, false);
       return true;
     });
+  } else {
+    try {
+      const isAvailable = await pingUrlWithTCP(check);
+      if (isAvailable) {
+        await markReport(check, true);
+      } else {
+        await markReport(check, false);
+      }
+    } catch (error) {
+      console.error(`An error occurred: ${error}`);
+    }
   }
+}
+
+async function pingUrlWithTCP (check) {
+  const { url, port, timeout } = check;
+  return new Promise((resolve, reject) => {
+    const startTime = new Date();
+    const client = net.connect({ host: url, port }, () => {
+      // If the connection is successful, the site is available
+      client.end();
+      const endTime = new Date();
+      const responseTime = endTime - startTime;
+      check.responseTime = responseTime;
+      resolve(true);
+    });
+
+    client.on('error', (err) => {
+      console.log(err);
+      // If an error occurs, the site is not available
+      const endTime = new Date();
+      const responseTime = endTime - startTime;
+      check.responseTime = responseTime;
+      resolve(false);
+    });
+
+    // Set a custom timeout for the connection attempt
+    client.setTimeout(timeout, () => {
+      client.destroy(); // Close the connection
+      const endTime = new Date();
+      const responseTime = endTime - startTime;
+      check.responseTime = responseTime;
+      resolve(false);
+    });
+  });
 }
 
 module.exports = { pingUrl };
